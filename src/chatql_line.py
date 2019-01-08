46a53fc87fccad9197b7f3bcb225e9d1705ddc30
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-01-08 23:52:54 +0900

    Ignore test access token error

--- src/chatql_line.py
+++ src/chatql_line.py
@@ -39,6 +39,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     """Text Message Handler."""
+    if event.reply_token == '00000000000000000000000000000000':
+        return
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
