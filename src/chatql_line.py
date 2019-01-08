27c6a7c3a7c992764c098ea5e4f6db931f4762db
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-01-08 23:38:21 +0900

    Change log level

--- src/chatql_line.py
+++ src/chatql_line.py
@@ -13,13 +13,13 @@ app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ.get('LINE_CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.environ.get('LINE_CHANNEL_SECRET'))
+app.logger.setLevel(logging.INFO)
 
 
 @app.route("/callback", methods=['POST'])
 def callback():
     """Line Webhook interface"""
     # get X-Line-Signature header value
-    app.logger.info(request.headers)
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
